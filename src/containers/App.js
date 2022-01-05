7594f7fb4fed818e1025af958fac346e23ef089d
Nada Stojanovic <nadastojanovic@Nadas-MacBook-Air.local>
2022-01-06 00:30:28 +0100

    changed classes to React hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import '../containers/App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-class App extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			users: [],
-			searchfield: ''
-		}
-	}
+function App() {
 
-	componentDidMount() {
+	const [users, setUsers] = useState([])
+	const [searchfield, setSearchfield] = useState('')
+
+	useEffect(()=> {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
-		.then(users => this.setState({ users: users}))
-	}
+		.then(users => {setUsers(users)});
+	},[])
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value)
 	}
-
-	render() {
-		const filteredUsers = this.state.users.filter(user => {
-			return user.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		})
-		return (
-			<div className='tc'>
-				<h1 className='f1'>Big City Paws</h1>
-				<div
-				style={{background: 'url(bg.jpeg) center',
-  				backgroundSize: 'cover'}}
-  				>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<ErrorBoundary>
-							<CardList users = {filteredUsers}/>	
-						</ErrorBoundary>
-					</Scroll>
-				</div>
+	const filteredUsers = users.filter(user => {
+		return user.name.toLowerCase().includes(searchfield.toLowerCase());
+	})
+	return (
+		<div className='tc'>
+			<h1 className='f1'>Big City Paws</h1>
+			<div
+			style={{background: 'url(bg.jpeg) center',
+				backgroundSize: 'cover'}}
+				>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<ErrorBoundary>
+						<CardList users = {filteredUsers}/>	
+					</ErrorBoundary>
+				</Scroll>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export default App;
